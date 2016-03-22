1b90259ad819a7a05700e8d488f2155dd50c579b
Tim OConnell <tuc.github@uaconaill.com>
2016-03-21 22:40:16 -0400

    Updated fileutils

--- Source/ua/utils/fileutils.py
+++ Source/ua/utils/fileutils.py
@@ -1,5 +1,10 @@
 import os
 
+def file_exists(file_path):
+    
+    exists = os.path.exists(file_path)  
+    return exists
+
 def file_extension (file_name):
     
     file_bare_name, file_ext = os.path.splitext(file_name)
--- Source/uautils/fileutils.py
+++
@@ -1,6 +0,0 @@
-import os
-
-def file_exists(file_path):
-    
-    exists = os.path.exists(file_path)  
-    return exists
