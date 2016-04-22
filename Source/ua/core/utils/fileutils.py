82fb0a99a25158a3aaab33d44d3573a545c57b27
Tim OConnell <tuc.github@uaconaill.com>
2016-04-22 07:07:33 -0400

    Added file_base_name to fileutils

--- Source/ua/core/utils/fileutils.py
+++ Source/ua/core/utils/fileutils.py
@@ -26,7 +26,12 @@ def is_file_exists(file_path):
     exists = os.path.exists(file_path)  
     return exists
 
+def file_base_name (file_name):
+    
+    file_base_name, file_ext = os.path.splitext(file_name)
+    return file_base_name
+
 def file_extension (file_name):
     
-    file_bare_name, file_ext = os.path.splitext(file_name)
+    file_base_name, file_ext = os.path.splitext(file_name)
     return file_ext
