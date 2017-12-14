b7cc4a57697fae9a06de7c7213b00843344e1612
Tim OConnell <tuc.github@uaconaill.com>
2017-12-14 13:51:08 -0500

    Added to dateutils.

--- Source/ua/core/utils/dateutils.py
+++ Source/ua/core/utils/dateutils.py
@@ -1,5 +1,10 @@
 from datetime import datetime
+from datetime import timedelta
 import time
+
+DATE_FORMAT = '%Y.%m.%d'
+DATE_SEPARATOR = '.'
+
 
 def datetime_now():
     return datetime.today()
@@ -8,4 +13,51 @@ def epoch_seconds_now():
     return time.time()
 
 def is_datetime_within (actual_datetime, test_datetime, test_time_range):
+    ''' Determin if the date is within a date range (inclusive check).
+    '''
+    
     return abs(actual_datetime - test_datetime) <= test_time_range
+
+def to_date_from_string (date_string):
+    
+    date_string = date_string.replace ('/', DATE_SEPARATOR)
+    date_string = date_string.replace ('-', DATE_SEPARATOR)
+    
+    date_actual = datetime.strptime (date_string, DATE_FORMAT)
+    
+    return date_actual
+
+def to_date_string (date):
+    
+    return date.strftime (DATE_FORMAT)
+
+
+def week_end_date (self, week_start_date):
+    ''' Calculate the end date of the week given the start date.
+    '''
+
+    timediff = timedelta (days = 6)
+    week_end_date = week_start_date + timediff
+    
+    return week_end_date
+
+def week_number (start_date, end_date):
+    ''' Calculates the week number of the given end date from the date
+        of the first day of the first week.
+    '''
+    
+    timediff = end_date - start_date
+    week_number = 1 + (timediff.days // 7)
+    
+    return week_number
+
+def week_start_date (self, start_date, week_number):
+    ''' Return the start date of the week number given the original
+        start date of the first week.
+    '''
+
+    timediff = timedelta (days = 7 * (week_number - 1))
+    week_start_date = (start_date + timediff).date()
+    
+    return week_start_date
+
--- Source/ua/core/utils/test/test_dateutils.py
+++ Source/ua/core/utils/test/test_dateutils.py
@@ -24,3 +24,18 @@ class Test_DateUtils(unittest.TestCase):
 
     def test_date_border_time_within_range(self):
         self.assertTrue(dateutils.is_datetime_within(TEST_DATE_1, TEST_DATE_2, TEST_DELTA_1_SECOND))
+        
+    def test_week_number_same_date(self):
+        self._call_and_test_week_number('2001-01-01', '2001-01-01', 1)
+
+    def test_week_number_next_week(self):
+        self._call_and_test_week_number('2001-01-01', '2001-01-08', 2)
+
+    def test_week_number_next_week_2(self):
+        self._call_and_test_week_number('2001-01-01', '2001-01-14', 2)
+
+    def _call_and_test_week_number (self, start_date_string, end_date_string, expected):
+        actual = dateutils.week_number (dateutils.to_date_from_string (start_date_string), dateutils.to_date_from_string (end_date_string))
+        self.assertEquals (expected, actual)
+
+
